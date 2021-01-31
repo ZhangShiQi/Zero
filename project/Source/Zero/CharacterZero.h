73093135eb4c71867b968f51e8d83f7aed03fc0c
ZhangShiQi <287542287@qq.com>
2021-01-31 13:11:30 +0800

    first commit

---
+++ project/Source/Zero/CharacterZero.h
@@ -0,0 +1,27 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperCharacter.h"
+#include "CharacterZero.generated.h"
+
+/**
+ * 
+ */
+UCLASS(BlueprintType|BlueprintClass)
+class ZERO_API ACharacterZero : public APaperCharacter
+{
+	GENERATED_BODY()
+	
+public:
+	ACharacterZero();
+	virtual ~ACharacterZero();
+
+public:
+
+
+
+
+
+
+};
---
+++ project/Source/Zero/ZeroGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ZeroGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ZERO_API AZeroGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
